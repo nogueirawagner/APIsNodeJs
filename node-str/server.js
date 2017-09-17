aa4428b617f6bd9cbc3ca39af616e55a391f2db4
Wagner Nogueira <wagnogueira19@hotmail.com>
2017-09-17 11:02:34 -0300

    servidor

--- node-str/server.js
+++ node-str/server.js
@@ -11,8 +11,7 @@ app.set('port', port);
 const server = http.createServer(app);
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) =>
-{
+const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store Api",
         version: "0.0.1"
@@ -21,18 +20,40 @@ const route = router.get('/', (req, res, next) =>
 app.use('/', route);
 
 server.listen(port);
-console.log('Deu certo ' + port);
+server.on('error', onError);
+console.log('Rodando na porta ' + port);
 
 // Normalizar porta
-function normalizePort(valor)
-{
+function normalizePort(valor) {
     const port = parseInt(valor, 10);
 
-    if(isNaN(port))
+    if (isNaN(port))
         return val;
-    
-    if(port >= 0)
+
+    if (port >= 0)
         return port;
-    
+
     return false;
+}
+
+// Funções para tratar erro
+function onError(error) {
+    if (error.syscall !== 'listen')
+        throw error;
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requerido privilegios elevados');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' está em uso');
+            process.exit(1);
+        default:
+            throw error;
+    }
 }
