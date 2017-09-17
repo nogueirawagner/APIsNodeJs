3d2a6d789238182191a2ea91f2e0ce6876c2e0c9
Wagner Nogueira <wagnogueira19@hotmail.com>
2017-09-17 19:47:33 -0300

    criando crud put, create, del, get

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -1,8 +1,13 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -12,14 +17,25 @@ const route = router.get('/', (req, res, next) => {
 });
 
 const create = router.post('/', (req, res, next) => {
-    res.status(201).send();
+    res.status(201).send(req.body);
 });
 
-const read = router.read('/', (req, res) => {
-    res.status(200).send();
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(201).send({
+        id: id,
+        item: req.body
+    });
 });
+
+const del = router.delete('/', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
 
 app.use('/', route);
 app.use('/produtos', create);
+app.use('/produtos', put);
+app.use('/produtos', del);
 
 module.exports = app;
