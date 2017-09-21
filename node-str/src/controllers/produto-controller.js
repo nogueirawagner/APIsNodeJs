761d1d76a6650b83c3aa6ce124b2429c0e1579b8
Wagner Nogueira <wagnogueira19@hotmail.com>
2017-09-20 23:27:56 -0300

    Listando produto

--- node-str/src/controllers/produto-controller.js
+++ node-str/src/controllers/produto-controller.js
@@ -3,6 +3,54 @@
 const mongoose = require('mongoose');
 const Produto = mongoose.model('Produto')
 
+exports.listarPorMarca = (req, res) => {
+	Produto.find(
+		{
+			marca: req.params.marca,
+			ativo: true
+		},
+		'codigo titulo preco marca')
+		.then(data => {
+			res.status(200).send(data);
+		}).catch(e => {
+			res.status(400).send(e);
+		});
+}
+
+exports.listarPorID = (req, res) => {
+	Produto.findById(req.params.id)
+		.then(data => {
+			res.status(200).send(data);
+		}).catch(e => {
+			res.status(400).send(e);
+		});
+}
+
+exports.listarProdCodigo = (req, res) => {
+	Produto.findOne(
+		{
+			codigo: req.params.codigo,
+			ativo: true
+		},
+		'codigo titulo preco marca')
+		.then(data => {
+			res.status(200).send(data);
+		}).catch(e => {
+			res.status(400).send(e);
+		});
+}
+
+exports.listarProdutos = (req, res) => {
+	Produto.find(
+		{ ativo: true }, // Traz somente os ativos
+		'titulo preco') // Exibe somente as colunas titulo e preco
+		.then(data => {
+			res.status(200).send(data);
+		}).catch(e => {
+			res.status(400).send(e);
+		});
+}
+
 exports.post = (req, res) => {
 	var prod = new Produto();
 	prod.titulo = req.body.titulo;
@@ -11,14 +59,14 @@ exports.post = (req, res) => {
 	prod.preco = req.body.preco;
 	prod.marca = [req.body.marca + " preta", req.body.marca + " amarela"];
 	prod.save()
-	.then(x => {
-		res.status(201).send({mensagem: 'Produto cadastrado com sucesso'});
-	}).catch(e => {
-		res.status(400).send({
-			mensagem: 'Falha ao cadastrar o produto',
-			data: e 
+		.then(x => {
+			res.status(201).send({ mensagem: 'Produto cadastrado com sucesso' });
+		}).catch(e => {
+			res.status(400).send({
+				mensagem: 'Falha ao cadastrar o produto',
+				data: e
+			});
 		});
-	});
 }
 
 exports.put = (req, res) => {
--- node-str/src/routes/produto-route.js
+++ node-str/src/routes/produto-route.js
@@ -4,6 +4,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/produto-controller');
 
+router.get('/', controller.listarProdutos);
+router.get('/:codigo', controller.listarProdCodigo);
+router.get('/admin/:id', controller.listarPorID);
+router.get('/marca/:marca', controller.listarPorMarca);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
