ff6d5a8236f29f5f1f26a5f05eafbc7b6cd2c2ed
Wagner Nogueira <wagnogueira19@hotmail.com>
2017-10-04 21:18:40 -0300

    Incluindo next

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -29,10 +29,11 @@ app.use(bodyParser.urlencoded({
 }));
 
 // Habilita CORS
-app.use(function (req, res) {
+app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, x-access-token');
     res.header('Acess-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
 });
 
 app.use('/', indexRoute);
